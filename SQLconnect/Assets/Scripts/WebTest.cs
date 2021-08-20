0a440992f07b619d96f78840c51dfaa6b012346b
lohannahme <61636785+lohannahme@users.noreply.github.com>
2021-08-20 12:35:55 -0300

    Files

---
+++ SQLconnect/Assets/Scripts/WebTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW request = new WWW("http://localhost/sqlconnect/webtest.php");
+        yield return request;
+        Debug.Log(request.text);
+    }
+
+}
